5b725ad11516201a0d27baa0f28d4d9892476427
Elizabeth Lam <43484130+eli-lam@users.noreply.github.com>
2020-12-04 23:54:17 -0500

    Import target select sprite

---
+++ Assets/Scripts/Combat/MouseSelect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSelect : MonoBehaviour
+{
+    public GameObject selectedObject;
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo)) {
+            GameObject hitObject = hitInfo.transform.root.gameObject;
+        }
+        else 
+        {
+            ClearSelection();
+        }
+    }
+
+    void SelectTarget(GameObject obj)
+    {
+        if (selectedObject != null)
+        {
+            if (obj == selectedObject)
+                return;
+
+            ClearSelection();
+        }
+    }
+
+    void ClearSelection()
+    {
+        selectedObject = null;
+    }
+}
